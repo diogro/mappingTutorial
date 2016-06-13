2b9f6244f3b61f2d6f937de59073145c5c683d72
Diogo Melo <diogro@gmail.com>
2016-06-13 14:39:26 +0100

    outlier plots

--- R/3_diagnostics_plots.R
+++ R/3_diagnostics_plots.R
@@ -8,12 +8,15 @@ save_plot("output/figures/unscaled_area.png", unscaled_area_plot,
 
 # Outlier detection
 # http://www.statistik.tuwien.ac.at/forschung/CS/CS-2006-3complete.pdf
-area_array = area_data %>% select(area1:area7)
+area_array = select(area_data, area1:area7)
 x.out = pcout(area_array, makeplot = TRUE)
 aq.plot(area_array)
 
-# Scaled data:
+## There is also a function to manualy remove outliers in the qq-plot
+# par(mfrow = c(1, 1))
+# chisq.plot(area_array)
 
+# Scaled data:
 scaled_area_plot = ggplot(n_area_data_std, aes(trait, value)) +
   geom_violin() + geom_jitter(alpha = 0.3, height = 0, width = 0.1) +
   facet_wrap(~SEX)
